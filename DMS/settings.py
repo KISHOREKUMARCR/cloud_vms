0ece2e42e31b77056823f50f14e40073ca2b002b
KISHOREKUMARCR <cosai.kishore@gmail.com>
2024-05-31 17:55:23 +0530

    testing_domain

--- DMS/settings.py
+++ DMS/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from .config import *
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -193,3 +193,4 @@ Video_File_Threshold='1 GB'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+django_heroku.settings(locals())
