5d99f794b638ae8081b1fa4e091be66fea0339a4
KISHOREKUMARCR <cosai.kishore@gmail.com>
2024-06-03 12:57:57 +0530

    updates_wsgi

--- DMS/settings.py
+++ DMS/settings.py
@@ -73,6 +73,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 ROOT_URLCONF = "DMS.urls"
 
 # adding project_root to simplify migration to another system
@@ -158,11 +161,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
--- DMS/wsgi.py
+++ DMS/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DMS.settings")
 
 application = get_wsgi_application()
+
+# Apply WhiteNoise to the WSGI application
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
