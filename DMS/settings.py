0a3c18e8787e39a545e80f479cff9e9b5e6aa407
KISHOREKUMARCR <cosai.kishore@gmail.com>
2024-06-05 19:53:11 +0530

    static_files_updates

--- DMS/settings.py
+++ DMS/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-        
+    "django.contrib.staticfiles.middleware.StaticFilesMiddleware",       
 
 ]
 
@@ -172,7 +172,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 
 ## Media files (images) within django
