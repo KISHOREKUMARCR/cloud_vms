f4dc579fa9510d31e9c16fd62d944461fb58a365
KISHOREKUMARCR <cosai.kishore@gmail.com>
2024-06-05 19:56:50 +0530

    remove_whitenoisemiddelware

--- DMS/settings.py
+++ DMS/settings.py
@@ -67,8 +67,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.contrib.staticfiles.middleware.StaticFilesMiddleware",       
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",  
 
 ]
 
